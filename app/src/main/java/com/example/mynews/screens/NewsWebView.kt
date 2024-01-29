ec78638a1e75f754e12941eafe032c4c4d4f9640
Richa <rd37574@gmail.com>
2024-01-30 01:30:24 +0530

    Initial1

--- app/src/main/java/com/example/mynews/screens/NewsWebView.kt
+++ app/src/main/java/com/example/mynews/screens/NewsWebView.kt
@@ -10,19 +10,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
@@ -30,25 +23,16 @@ import androidx.compose.ui.viewinterop.AndroidView
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NewsWebView(url: String, onBackPress: () -> Unit) {
-    var isLoading by remember { mutableStateOf(true) }
-
     Column {
         TopAppBar(
             navigationIcon = {
                 IconButton(onClick = { onBackPress.invoke() }) {
-                    Icon(Icons.Default.ArrowBack, contentDescription = "Back", modifier = Modifier.padding(top = 6.dp))
+                    Icon(Icons.Default.ArrowBack, contentDescription = "Back",modifier = Modifier.padding(top = 6.dp))
                 }
             },
-            title = { Text(text = "News Article", modifier = Modifier.padding(top = 16.dp)) },
+            title = { Text(text = "News Article", modifier = Modifier.padding(top = 16.dp))},
             modifier = Modifier.height(56.dp)
         )
-
-        if (isLoading) {
-            // Show circular loading indicator while the article is loading
-            CircularProgressIndicator(modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .padding(16.dp))
-        }
 
         AndroidView(
             factory = { context ->
@@ -58,13 +42,7 @@ fun NewsWebView(url: String, onBackPress: () -> Unit) {
                         ViewGroup.LayoutParams.MATCH_PARENT
                     )
                     settings.javaScriptEnabled = true
-                    webViewClient = object : WebViewClient() {
-                        override fun onPageFinished(view: WebView?, url: String?) {
-                            super.onPageFinished(view, url)
-                            // Article loading is complete, set isLoading to false
-                            isLoading = false
-                        }
-                    }
+                    webViewClient = WebViewClient()
                     loadUrl(url)
                 }
             },
@@ -73,7 +51,8 @@ fun NewsWebView(url: String, onBackPress: () -> Unit) {
                     if (keyCode == KeyEvent.KEYCODE_BACK && event.action == MotionEvent.ACTION_UP && webView.canGoBack()) {
                         webView.goBack()
                         true
-                    } else {
+                    }
+                    else {
                         false
                     }
                 }
